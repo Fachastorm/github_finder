96cc2fad5f32f2a877d373827bef9691dbda884e
Fachastorm <rodrigo.lodeiro@live.com>
2018-07-17 13:41:05 -0400

    added repos and finished styling

--- github.js
+++ github.js
@@ -1,17 +1,24 @@
 class GitHub { 
   constructor(){ 
     this.client_id = 'a6d42efda965570762c9';
-    this.client_secret = '7ef7db629e0bcde386bc62d96101a054b90ec943'; 
+    this.client_secret = '7ef7db629e0bcde386bc62d96101a054b90ec943';
+    this.repos_count = 5; 
+    this.repos_sort = 'created: asc';
   }
 
 
   async getUser(user){ 
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const profile = await profileResponse.json(); 
+    const profile = await profileResponse.json();
+
+    const repos = await repoResponse.json()
 
     return { 
-      profile   //No need for the second if using ES6 profile: profile.
+      profile,   //No need for the second if using ES6 profile: profile.
+      repos
     }
   }
 }
